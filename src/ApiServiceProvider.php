92a0f63c9f57e1a1f0f1ce68b3ef1c986e822e26
daivietcom <63624549+daivietcom@users.noreply.github.com>
2020-09-23 09:05:20 +0700

    Upload

---
+++ src/ApiServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LumenApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
